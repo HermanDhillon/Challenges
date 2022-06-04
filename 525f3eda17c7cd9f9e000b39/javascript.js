f49259714c05f476ca4cb0b21331131d2366b3db
H-Dhill <dhillonherman1991@gmail.com>
2022-06-04 21:34:26 +0000

    Add "Calculating with Functions" JavaScript solution

---
+++ 525f3eda17c7cd9f9e000b39/javascript.js
@@ -0,0 +1,66 @@
+function zero(inp="x") {
+  if(inp !== "x"){
+    return ~~eval(0+inp)
+  }else{
+    return 0
+  }
+}
+function one(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(1+inp)
+  }else{
+    return 1
+  }}
+function two(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(2+inp)
+  }else{
+    return 2
+  }}
+function three(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(3+inp)
+  }else{
+    return 3
+  }}
+function four(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(4+inp)
+  }else{
+    return 4
+  }}
+function five(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(5+inp)
+  }else{
+    return 5
+  }}
+function six(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(6+inp)
+  }else{
+    return 6
+  }}
+function seven(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(7+inp)
+  }else{
+    return 7
+  }}
+function eight(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(8+inp)
+  }else{
+    return 8
+  }}
+function nine(inp="x") {  
+  if(inp !== "x"){
+    return ~~eval(9+inp)
+  }else{
+    return 9
+  }}
+
+function plus(inp) {return "+" + inp}
+function minus(inp) {return "-" + inp}
+function times(inp) {return "*" + inp}
+function dividedBy(inp) {return "/" + inp}
