0323fd5ee306332c976cdd7c043812e2cabcc8ff
H-Dhill <dhillonherman1991@gmail.com>
2022-06-04 22:14:24 +0000

    Add "Return the day " JavaScript solution

---
+++ 59dd3ccdded72fc78b000b25/javascript.js
@@ -0,0 +1,7 @@
+function whatday(num) { 
+
+ return num > 0 && num <8 ? [ "Sunday","Monday","Tuesday" ,"Wednesday","Thursday","Friday","Saturday"][num-1] : 'Wrong, please enter a number between 1 and 7'
+
+
+
+}
